a76df11b0087367c8c3145bae22ca0c898e7988d
Dmitry_Neustupov <neustupov@yandex.ru>
2020-02-09 20:36:09 +0300

    Added more service tests

--- src/test/java/ru/neustupov/demolibrary/service/BookServiceTest.java
+++ src/test/java/ru/neustupov/demolibrary/service/BookServiceTest.java
@@ -3,16 +3,17 @@ package ru.neustupov.demolibrary.service;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 import ru.neustupov.demolibrary.model.Book;
 import ru.neustupov.demolibrary.model.Level;
 import ru.neustupov.demolibrary.model.Rack;
 
 @SpringBootTest
+@Transactional
 class BookServiceTest {
 
   @Autowired
@@ -23,53 +24,55 @@ class BookServiceTest {
 
   @BeforeEach
   void setUp() {
-    Rack rack1 = rackService.save(new Rack());
-    Rack rack2 = rackService.save(new Rack());
-    bookService.save(Book.builder().rack(rack1).level(Level.ONE1).name("AAA")
-        .build());
-    bookService.save(Book.builder().rack(rack1).level(Level.ONE1).name("BBB")
-        .build());
-    bookService.save(Book.builder().rack(rack2).level(Level.TWO2).name("CCC")
-        .build());
-  }
-
-  @AfterEach
-  void tearDown() {
     bookService.deleteAll();
     rackService.deleteAll();
   }
 
   @Test
   void getBookById() {
-    assertEquals(bookService.getBookById(1L).get().getId(), 1L);
+    Rack rack1 = rackService.save(new Rack());
+    Book book = bookService.save(Book.builder().rack(rack1).level(Level.ONE1).name("AAA")
+        .build());
+    assertEquals(bookService.getBookById(book.getId()).get().getId(), book.getId());
   }
 
   @Test
   void getBookByName() {
+    Rack rack1 = rackService.save(new Rack());
+    bookService.save(Book.builder().rack(rack1).level(Level.ONE1).name("AAA")
+        .build());
     assertEquals(bookService.getBookByName("AAA").getName(), "AAA");
   }
 
   @Test
   void getAllByLevel() {
+    Rack rack1 = rackService.save(new Rack());
+    bookService.save(Book.builder().rack(rack1).level(Level.ONE1).name("AAA")
+        .build());
     List bookList = bookService.getAllByLevel(Level.ONE1);
-    assertEquals(bookList.size(), 2);
+    assertEquals(bookList.size(), 1);
   }
 
   @Test
   void getAllByRack() {
+    Rack rack1 = rackService.save(new Rack());
+    bookService.save(Book.builder().rack(rack1).level(Level.ONE1).name("AAA")
+        .build());
     Rack rack = rackService.getRackById(1L).get();
-    assertEquals(bookService.getAllByRack(rack).size(), 2);
+    assertEquals(bookService.getAllByRack(rack).size(), 1);
   }
 
   @Test
   void getAllByRackAndLevel() {
-    Rack rack = rackService.getRackById(1L).get();
-    assertEquals(bookService.getAllByRackAndLevel(rack, Level.ONE1).size(), 2L);
+    Rack rack1 = rackService.save(new Rack());
+    bookService.save(Book.builder().rack(rack1).level(Level.ONE1).name("AAA")
+        .build());
+    assertEquals(bookService.getAllByRackAndLevel(rack1, Level.ONE1).size(), 1);
   }
 
   @Test
   void save() {
-    Rack rack = rackService.getRackById(1L).get();
+    Rack rack = rackService.save(new Rack());
     Book book = Book.builder().name("ZZZ").rack(rack).level(Level.THREE3).build();
     bookService.save(book);
     assertEquals(bookService.getBookByName("ZZZ").getName(), "ZZZ");
@@ -77,8 +80,21 @@ class BookServiceTest {
 
   @Test
   void deleteById() {
-    Book book = bookService.getBookById(1L).get();
+    Rack rack1 = rackService.save(new Rack());
+    Book book = bookService.save(Book.builder().rack(rack1).level(Level.ONE1).name("AAA")
+        .build());
+    bookService.save(Book.builder().rack(rack1).level(Level.ONE1).name("BBB")
+        .build());
     bookService.deleteById(book);
     assertEquals(bookService.getAllByLevel(Level.ONE1).size(), 1);
+  }
+
+  @Test
+  void deleteAll() {
+    Rack rack1 = rackService.save(new Rack());
+    bookService.save(Book.builder().rack(rack1).level(Level.ONE1).name("AAA")
+        .build());
+    bookService.deleteAll();
+    assertEquals(bookService.getAllByLevel(Level.ONE1).size(), 0);
   }
 }
--- src/test/java/ru/neustupov/demolibrary/service/RackServiceTest.java
+++ src/test/java/ru/neustupov/demolibrary/service/RackServiceTest.java
@@ -2,24 +2,44 @@ package ru.neustupov.demolibrary.service;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.NoSuchElementException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import ru.neustupov.demolibrary.model.Rack;
 
+@SpringBootTest
 class RackServiceTest {
+
+  @Autowired
+  private RackService rackService;
+
+  @Autowired
+  private BookService bookService;
 
   @BeforeEach
   void setUp() {
+    bookService.deleteAll();
+    rackService.deleteAll();
   }
 
   @Test
   void getRackById() {
+    Rack rack = rackService.save(new Rack());
+    assertEquals(rackService.getRackById(rack.getId()).get().getId(), rack.getId());
   }
 
   @Test
   void save() {
+    Rack rack = rackService.save(new Rack());
+    assertEquals(rackService.getRackById(rack.getId()).get().getId(), rack.getId());
   }
 
   @Test
   void deleteAll() {
+    Rack rack = rackService.save(new Rack());
+    rackService.deleteAll();
+    assertThrows(NoSuchElementException.class, () -> rackService.getRackById(rack.getId()).get());
   }
 }
